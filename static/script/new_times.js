c916ccc5f7c16c9961969339e2135e4f32a9b607
Ismail Sacic <cicas03@hotmail.com>
2023-11-22 21:21:23 +0100

    Update new_times.js

--- static/script/new_times.js
+++ static/script/new_times.js
@@ -12,31 +12,24 @@ const check_if_contains = (array, value) => {
     return false;
 }
 
-const vaktija_eu = async () =>    {
-    let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+const vaktija_eu = async () => {
     console.log("Fetching vaktija.eu");
-    response = await fetch("/get_vaktija_eu");
-    data = await response.json();
+    let response = await fetch("/get_vaktija_eu");
+    let data = await response.json();
     if (data.error) {
         alert("Error fetching vaktija.eu\nPlease try again later.\nIf the problem still exists, contact me cicas03@hotmail.com");
-    }
-    else {
-        for(let i = 0; i < data.data.length; i++) {
-            let vaktija = data.data[i];
-            let vaktija_slug = vaktija.slug;
-            let vaktija_name = vaktija.name;
-            let vaktija_country_code = vaktija.country.short_code;
-            let full_country_name = regionNames.of(vaktija_country_code);
-            if (check_if_contains(countries, full_country_name) != true) {
-                countries.push({
-                    "name": full_country_name,
-                    "cities": [
-                        {"name": vaktija_name, "slug": vaktija_slug}
-                    ]
-                });
-            }
-            else {
-                countries.find(x => x.name == full_country_name).cities.push({"name": vaktija_name, "slug": vaktija_slug});
+    } else {
+        let countries = [];
+        for (let i = 0; i < data.data.length; i++) {
+            let city = data.data[i];
+            let country = city.country.name;
+            let cityObj = { "name": city.name, "slug": city.slug };
+
+            let countryIndex = countries.findIndex(c => c.name === country);
+            if (countryIndex === -1) {
+                countries.push({ "name": country, "cities": [cityObj] });
+            } else {
+                countries[countryIndex].cities.push(cityObj);
             }
         }
         console.log("Successfully fetched vaktija.eu");
@@ -48,28 +41,23 @@ const vaktija_eu = async () =>    {
             option.text = countries[i].name;
             option.value = countries[i].name;
             select.appendChild(option);
-            select.add(option);
         }
         select.addEventListener("change", function() {
-                let cities = countries.find(x => x.name == this.value).cities;
-                let select_city = document.getElementById("vaktija_eu_city");
-                let vaktija_name = document.getElementById("vaktija_eu_name");
-                vaktija_name.style.display = "block";
-                select_city.style.display = "block";
-                select_city.innerHTML = "";
+            let cities = countries.find(x => x.name === this.value).cities;
+            let select_city = document.getElementById("vaktija_eu_city");
+            let vaktija_name = document.getElementById("vaktija_eu_name");
+            vaktija_name.style.display = "block";
+            select_city.style.display = "block";
+            select_city.innerHTML = "";
 
-                for(let i = 0; i < cities.length; i++) {
-                    let option = document.createElement("option");
-                    option.text = cities[i].name;
-                    option.value = cities[i].slug;
-                    select_city.appendChild(option);
-                    select_city.add(option);
-                }
-
-
+            for (let i = 0; i < cities.length; i++) {
+                let option = document.createElement("option");
+                option.text = cities[i].name;
+                option.value = cities[i].slug;
+                select_city.appendChild(option);
+            }
         });
     }
-
 }
 vaktija_eu();
 
